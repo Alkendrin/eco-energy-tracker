5f2f409ccff6983548d4216094b1ba930ecebe6c
Alkendrin <104898124+Alkendrin@users.noreply.github.com>
2025-06-04 13:14:27 +0800

    fix

--- Start.bat
+++ Start.bat
@@ -1,18 +1,4 @@
+
 @echo off
-:: filepath: c:\Users\User\Desktop\CAPSTONE\eco-energy-tracker\Start.bat
-echo Starting EcoEnergy Tracker...
-
-:: Check if virtual environment exists, if not create it
-if not exist env (
-    echo Setting up environment for first run...
-    python setup.py
-)
-
-:: Activate virtual environment and start application
-call env\Scripts\activate.bat
-python main.py
-
-:: Open browser to application
-start http://127.0.0.1:5000
-
-echo EcoEnergy Tracker is running!
+start "" "http://127.0.0.1:5000"
+start /min /b pythonw main.py
